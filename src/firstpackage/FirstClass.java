4402d7a4638658e2a8485b8ea96178ae3f72df81
Sueheyla <sueheylatas@gmail.com>
2020-06-21 15:12:02 +0200

    son conflikt bugünlük

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,10 +5,8 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("first commit from local repository");
 		System.out.println("second commit from local repository");
-
 		
-
-		System.out.println("Conflikt istiyoruz");
+		System.out.println("2.kez conflikt yapiyoruz");
 
 		System.out.println("First commit from remote repository");
 		
