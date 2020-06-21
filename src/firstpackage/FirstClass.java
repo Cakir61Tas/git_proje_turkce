5f1cc49a6eac3ff4200551f0b4f5f7ca33ddfc2d
Cakir61Tas <67127922+Cakir61Tas@users.noreply.github.com>
2020-06-21 14:45:35 +0200

    Update FirstClass.java

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -6,6 +6,8 @@ public class FirstClass {
 		System.out.println("first commit from local repository");
 		System.out.println("second commit from local repository");
 		System.out.println("Third commit from Local repository");
+		System.out.println("First commit from remote repository");
+		
 	}
 
 }
